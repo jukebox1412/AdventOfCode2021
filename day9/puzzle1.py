1680598096b47ebaf82ee317c7d8559a1d1f8163
jukebox1412 <jukebox1412@gmail.com>
2022-04-23 12:45:08 -0600

    day 9

--- day9/puzzle1.py
+++ day9/puzzle1.py
@@ -3,7 +3,7 @@
 
 def main():
     map = list()
-    with open("test.txt") as file:
+    with open("input.txt") as file:
         i = 0
         for line in file:
             line = line.strip()
@@ -15,11 +15,15 @@ def main():
     roll_downs = []
     for i in range(0, len(map)):
         for j in range(0, len(map[0])):
-            roll_downs.append(determine_roll_down(i, j, map))
-    print(filterDistinctResults(roll_downs))
+            roll_downs.append(determine_roll_down(i, j, map, set()))
+    print(sumAndFilterDistinctResults(roll_downs))
 
 
-def determine_roll_down(i: int, j: int, map: list):
+def determine_roll_down(i: int, j: int, map: list, history: set):
+    # so we don't backtrack
+    new_history = set(history)
+    new_history.add((i, j))
+
     up = (i - 1, j) if i > 0 else None
     down = (i + 1, j) if i < len(map) - 1 else None
     left = (i, j - 1) if j > 0 else None
@@ -33,16 +37,28 @@ def determine_roll_down(i: int, j: int, map: list):
             if map[dir[0]][dir[1]] < current_lowest_value:
                 current_lowest_dir = dir
                 current_lowest_value = map[dir[0]][dir[1]]
+            elif map[dir[0]][dir[1]] == current_lowest_value and dir not in new_history:
+                possible_lower_value = determine_roll_down(dir[0], dir[1], map, new_history)[0]
+                if possible_lower_value < current_lowest_value:
+                    current_lowest_dir = dir
+                    current_lowest_value = possible_lower_value
 
+    
     if current_lowest_dir:
-        return determine_roll_down(current_lowest_dir[0], current_lowest_dir[1], map)
+        return determine_roll_down(current_lowest_dir[0], current_lowest_dir[1], map, new_history)
     else:
         return (current_lowest_value, (i, j))
 
-def filterDistinctResults(results):
-    ret = dict()
+
+def sumAndFilterDistinctResults(results):
+    filtered = dict()
     for result in results:
-        ret[result[1]] = result[0]
+        filtered[result[1]] = result[0]
+    print(filtered)
+    ret = 0
+    for key in filtered:
+        ret += filtered[key] + 1
     return ret
+
 
 main()
--- day9/puzzle2.py
+++ day9/puzzle2.py
@@ -1,41 +1,90 @@
-import statistics
+# https://adventofcode.com/2021/day/9
 
-from numpy import diff, number
-# https://adventofcode.com/2021/day/7
+
+from numpy import size
+
 
 def main():
-    my_input = []
+    map = list()
     with open("input.txt") as file:
+        i = 0
         for line in file:
-            split = line.strip().split(',')
-            for s in split:
-                my_input.append(int(s))
-    print(determine_fuel_cost(my_input))
+            line = line.strip()
+            map.append(list())
+            for chr in line:
+                map[i].append(int(chr))
+            i += 1
 
-fibonacci_numbers = dict()
+    roll_downs = []
+    for i in range(0, len(map)):
+        for j in range(0, len(map[0])):
+            roll_downs.append(determine_roll_down(i, j, map, set()))
 
-def determine_fuel_cost(my_input : list):
-    lowest_total_fuel_cost = 2147483647
-    for i in range(min(my_input), max(my_input) + 1):
-        total_fuel_cost = 0
-        for j in range(0, len(my_input)):
-            difference = abs(i - my_input[j])
-            fibo_diff = 0
-            if difference in fibonacci_numbers:
-                fibo_diff = fibonacci_numbers[difference]
-            else:
-                fibo_diff = find_fibo(difference)
-                fibonacci_numbers[difference] = fibo_diff
-            total_fuel_cost += fibo_diff
-        if total_fuel_cost < lowest_total_fuel_cost:
-            lowest_total_fuel_cost = total_fuel_cost
-    
-    return lowest_total_fuel_cost
+    filtered_roll_downs = filterDistinctResults(roll_downs)
+    print(filtered_roll_downs)
+    basin_set_sizes = list()
+    for roll_down in filtered_roll_downs:
+        basin_set = set()
+        find_basin(roll_down[1], map, basin_set)
+        basin_set_sizes.append(len(basin_set))
+    basin_set_sizes.sort()
+    basin_set_sizes.reverse()
+    print(basin_set_sizes[0] * basin_set_sizes[1] * basin_set_sizes[2])
 
-def find_fibo(difference: int):
-    total_difference = 0
-    for i in range(0, difference):
-        total_difference += i + 1
-    return total_difference
+
+def find_basin(start_dir, map: list, basin_set: set) -> int:
+    basin_set.add(start_dir)
+    up = (start_dir[0] - 1, start_dir[1]) if start_dir[0] > 0 else None
+    down = (start_dir[0] + 1, start_dir[1]
+            ) if start_dir[0] < len(map) - 1 else None
+    left = (start_dir[0], start_dir[1] - 1) if start_dir[1] > 0 else None
+    right = (start_dir[0], start_dir[1] +
+             1) if start_dir[1] < len(map[0]) - 1 else None
+
+    dirs = [up, down, left, right]
+    for dir in dirs:
+        if dir and map[dir[0]][dir[1]] < 9:
+            if dir not in basin_set:
+                find_basin(dir, map, basin_set)
+
+
+def determine_roll_down(i: int, j: int, map: list, history: set):
+    # so we don't backtrack
+    new_history = set(history)
+    new_history.add((i, j))
+
+    up = (i - 1, j) if i > 0 else None
+    down = (i + 1, j) if i < len(map) - 1 else None
+    left = (i, j - 1) if j > 0 else None
+    right = (i, j + 1) if j < len(map[0]) - 1 else None
+
+    dirs = [up, down, left, right]
+    current_lowest_dir = None
+    current_lowest_value = map[i][j]
+    for dir in dirs:
+        if dir:
+            if map[dir[0]][dir[1]] < current_lowest_value:
+                current_lowest_dir = dir
+                current_lowest_value = map[dir[0]][dir[1]]
+            elif map[dir[0]][dir[1]] == current_lowest_value and dir not in new_history:
+                possible_lower_value = determine_roll_down(
+                    dir[0], dir[1], map, new_history)[0]
+                if possible_lower_value < current_lowest_value:
+                    current_lowest_dir = dir
+                    current_lowest_value = possible_lower_value
+
+    if current_lowest_dir:
+        return determine_roll_down(current_lowest_dir[0], current_lowest_dir[1], map, new_history)
+    else:
+        return (current_lowest_value, (i, j))
+
+
+def filterDistinctResults(results):
+    filtered = dict()
+    for result in results:
+        filtered[result[1]] = result[0]
+
+    return [(filtered[key], key) for key in filtered]
+
 
 main()
