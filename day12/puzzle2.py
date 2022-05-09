a5181339eff9a8436addabbfc5e5de33c9a4d9c1
jukebox1412 <jukebox1412@gmail.com>
2022-05-09 16:50:31 -0600

    day12 part 1

---
+++ day12/puzzle2.py
@@ -0,0 +1,53 @@
+# https://adventofcode.com/2021/day/12
+import copy
+
+
+class Cave:
+    def __init__(self, is_small: bool, name: str):
+        self.is_small = is_small
+        self.name = name
+        self.paths = list()
+
+    def add_path(self, path_to):
+        self.paths.append(path_to)
+
+    def __str__(self):
+        return f"{self.name}: {[path.__str__() for path in self.paths]}"
+
+
+def main():
+    paths = dict()
+    with open("test.txt") as file:
+        for line in file:
+            line = line.strip()
+            splits = line.split('-')
+            left = splits[0]
+            right = splits[1]
+            if left not in paths:
+                paths[left] = Cave(left.islower(), left)
+            if right not in paths:
+                paths[right] = Cave(right.islower(), right)
+
+            paths[left].add_path(paths[right])
+            paths[right].add_path(paths[left])
+    solutions = []
+    find_path_to_end(paths['start'], ['start'], solutions=solutions)
+    for solution in solutions:
+        print_str = ""
+        for cave in solution:
+            print_str += cave + ','
+        print(print_str)
+    print(len(solutions))
+
+def find_path_to_end(cave: Cave, path_history: list, solutions: list):
+    if cave.name == 'end':
+        solutions.append(path_history)
+        return
+    for path in cave.paths: #type: Cave
+        number_of_times_visited = path_history.count(cave.name)
+        if not path.is_small or (path.name != 'start' and number_of_times_visited < 2):
+            new_path_history = list(path_history)
+            new_path_history.append(path.name)
+            find_path_to_end(path, new_path_history, solutions)
+
+main()
