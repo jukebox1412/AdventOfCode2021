a00523f3d510acf231bb6fa6b7e63e74b88a5f02
jukebox1412 <jukebox1412@gmail.com>
2022-05-20 20:26:42 -0600

    there's a bug but progress

--- day14/puzzle2.py
+++ day14/puzzle2.py
@@ -19,12 +19,10 @@ def main():
                     rules[split[0]] = split[0][0] + split[1] + split[0][1]
     cache = rules
     
-    for i in range(0, 2):
+    for i in range(0, 10):
         template = step(template, cache)
 
-    print(template)
-    for cached in cache:
-        print(f"[{cached}]:[{cache[cached]}]")
+    print(most_common_minus_least_common(template))
 
 def most_common_minus_least_common(template: str) -> int:
     counts = dict()
@@ -49,6 +47,7 @@ def step(template: str, cache: dict) -> str:
         for j in reversed(range(i + 1, min(len(template), i + MAX_CACHE_LENGTH))):
             if template[i:j+1] in cache:
                 jump_ahead = len(cache[template[i:j+1]])
+                print(f'Found in cache len {jump_ahead}')
                 template = template[:i] + cache[template[i:j+1]] + template[j+1:]
                 i += jump_ahead - 2
                 break # break because reversed and we need to jump ahead
@@ -56,12 +55,10 @@ def step(template: str, cache: dict) -> str:
 
     # update the cache
     for i in range(0, len(original_template) - 3): # minus 3 because we already have the len 2 combinations cached
-        for j in range(i + 3, min(i + MAX_CACHE_LENGTH, len(original_template))):
+        for j in range(i + 2, min(i + MAX_CACHE_LENGTH, len(original_template))):
             new_i = i + int((i + 1) / 2) 
-            new_j = j + int((j + 1) / 2)
-            cache[original_template[i:j]] = template[new_i:new_j + 1]
-
-            
+            new_j = j + int((j + 1) / 2) + 1
+            cache[original_template[i:j+1]] = template[new_i:new_j + 1]
 
     return template
             
